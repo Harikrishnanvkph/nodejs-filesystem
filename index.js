6f2600b066acfe75ef7381cfaaf5571dea90f2b8
HarikrishnanVK <54284966+Harikrishnanvkph@users.noreply.github.com>
2024-06-21 21:03:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ server.use("/read",require("./api_endpoint.js"))
 
 // listening to the server via port
 server.listen(port,()=>{
-  console.log(process.env)
     console.log(`Starting the Server at http://localhost:${port} ...`)
 })
 
@@ -22,14 +21,25 @@ server.listen(port,()=>{
 server.get("/",async (req,res,next)=>{
    res.json({
     success : true,
-    message : `Successfully written ${file}`,
-    available_Endpoints : ["/read","/read/date-time","/read/date-time1","/read/date-time2"]
+    message : `Welcome to Node JS Task 1 `,
+    available_Endpoints : ["/write","/read","/read/date-time","/read/date-time1","/read/date-time2"]
    })
 })
 
-function stampFile(){
-    // Get the current timestamp
-    var timestamp = Date.now();
-    var date = new Date()
-    return `Date is ${date}\nCurrent timestamp : ${timestamp}`;
+server.get("/write",async (req,res,next)=>{
+  console.log("got in")
+  const date = new Date();
+  const currentDateTime = date.toTimeString().split(" GMT+0530 (India Standard Time)")[0].split(":");
+  const [h,m,s] = currentDateTime;
+  const time = `${h}hrs_${m}min_${s}sec`;
+  await writeFile(`./date_stamp_files/${date.toDateString()} ${time}.txt`, await stampFile());
+  res.json(`Done written a file with current timestamp ${date} and with file name as "${date.toDateString()} ${time}.txt"`)
+})
+
+async function stampFile(){
+   // Get the current timestamp
+   var timestamp = Date.now();
+   var date = new Date()
+   return `Date is ${date}\nCurrent timestamp : ${timestamp}`;
 }
+
